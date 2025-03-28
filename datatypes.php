f674e8d9cab1eb628107f0490e90e84260ca43e8
RyANSpeED1 <155994820+RyANSpeED1@users.noreply.github.com>
2025-03-28 09:14:15 -0500

    Create datatypes.php

---
+++ datatypes.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Anything</title>
+</head>
+<body>
+<?php
+    //string
+     $name = "Running is hard";
+
+    //integer
+     $name = 5;
+    //float
+     $name = 5.5;
+    //boolean
+     true = 1
+     false = 0 
+    //array
+        $names = array("Madison", "Academic", "Track");
+        echo $names['3']; //Track
+?>
