279976fba3d901caf4d3d7faca76e4584163ea46
RyANSpeED1 <155994820+RyANSpeED1@users.noreply.github.com>
2025-03-31 09:14:18 -0500

    Create operators.php

---
+++ operators.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Anything</title>
+</head>
+<body>
+<?php
+   //arithmetic operators
+   echo 6**6;
+
+?>
+<?php
+   //assignment operators
+   $x = 5;
+   $x /= 10;
+   echo $x;
+?>
+<?php
+   //comparison operators
+   $x = 5;
+   $y = 10;
+   if ($x == $y) {
+       echo "True";
+   } else {
+       echo "Not equal";
+   }
+
+</body>
+</html>
